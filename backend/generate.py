ac5149866e6ca9bf95e7ea18328201bc8596d110
Philip <philipxbplan@gmail.com>
2024-06-17 16:51:35 +0200

    multiple image appending to site

--- backend/generate.py
+++ backend/generate.py
@@ -27,15 +27,19 @@ def generate():
     guidance_scale = data["guidance_scale"]
 
     #running function from the t2i script
-    image = run(image, prompt, style, amountOfImages, num_inference_steps, negative_prompt, adapter_conditioning_scale, guidance_scale)
+    images = run(image, prompt, style, amountOfImages, num_inference_steps, negative_prompt, adapter_conditioning_scale, guidance_scale)
+    
+    imgs = []
 
-    # Convert the processed image to base64
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    for img in images:
+        # Convert the processed images to base64
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        imgs.append(base64.b64encode(buffered.getvalue()).decode("utf-8"))
+
 
     # Return the base64 encoded image data as JSON
-    res = make_response(jsonify({"image": img_str}), 200)
+    res = make_response(jsonify({"images": imgs}), 200)
     return res
 
 if __name__ == "__main__":
