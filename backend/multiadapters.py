ede5fc2681027e75445b7768f7d62a8f726bf6e4
Philip <philipxbplan@gmail.com>
2024-07-20 12:01:46 +0200

    added connection between new script and generate.py/js

--- backend/multiadapters.py
+++ backend/multiadapters.py
@@ -9,7 +9,7 @@ import cv2
 import torchvision.transforms as transforms
 
 
-def run_multiadapter(image, prompt, amountOfImages, num_inference_steps, negative_prompt, adapter_conditioning_scale, guidance_scale):
+def run_multiadapter(image, colorpalette, prompt, amountOfImages, num_inference_steps, negative_prompt, adapter_conditioning_scale, guidance_scale):
 
     # load 2 adapters with new MultiAdapter function
     adapters = MultiAdapter(
@@ -23,12 +23,12 @@ def run_multiadapter(image, prompt, amountOfImages, num_inference_steps, negativ
     "https://huggingface.co/datasets/diffusers/docs-images/resolve/main/t2i-adapter/color_ref.png"
 )
 
-    color_image = Image.open("colorPalette.png")
+    color_image = colorpalette
     color_palette = color_image.resize((8, 8))
     color_palette = color_palette.resize((512, 512), resample=Image.Resampling.NEAREST) # pil image 512,512
     color_palette.save('color_test.png')
 
-    sketch_image = Image.open('images_sketch_in.png')
+    #sketch_image = Image.open('images_sketch_in.png')
 
     pipe = StableDiffusionAdapterPipeline.from_pretrained(
      "CompVis/stable-diffusion-v1-4",
@@ -47,7 +47,7 @@ def run_multiadapter(image, prompt, amountOfImages, num_inference_steps, negativ
     )'''
     #pidinet edge detection
     pidinet = PidiNetDetector.from_pretrained("lllyasviel/Annotators")
-    sketch_image = pidinet(sketch_image)
+    sketch_image = pidinet(image)
     sketch_image.save('weird_sketch.png')
     
 
@@ -85,9 +85,9 @@ def run_multiadapter(image, prompt, amountOfImages, num_inference_steps, negativ
         print(f"An error occurred during image generation: {e}")
 
 
-if __name__ == "__main__":
-    image = load_image("edgedetection1.png")
-    prompt = "A bed chamber with fancy bed and window"
-    run_multiadapter(image, prompt, amountOfImages = 1, num_inference_steps= 25, 
-                     negative_prompt = "extra digit, fewer digits, cropped, worst quality, low quality, glitch, deformed, mutated, ugly, disfigured",
-                    adapter_conditioning_scale = 0.6, guidance_scale = 7.5)
+#if __name__ == "__main__":
+#    image = load_image("edgedetection1.png")
+#    prompt = "A bed chamber with fancy bed and window"
+#    run_multiadapter(image, prompt, amountOfImages = 1, num_inference_steps= 25, 
+#                     negative_prompt = "extra digit, fewer digits, cropped, worst quality, low quality, glitch, deformed, mutated, ugly, disfigured",
+#                    adapter_conditioning_scale = 0.6, guidance_scale = 7.5)
